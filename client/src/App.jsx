e7a544c45d49d4ea90d625d700cca3533c79a309
Yash <yashgithug907@gmail.com>
2025-06-28 12:14:51 +0530

    replaced browserrouter with hash router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import UserForm from "./pages/UserForm";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/form/:eventId" element={<UserForm />} />
+        <Route path="/" element={<Login />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
     const [email, setEmail] = useState("admin@admin.com");
     const [password, setPassword] = useState("admin@1234");
+    const navigate = useNavigate()
     const baseURL = "https://dynamicformgenrater.onrender.com"
 
-    const handleLogin = async () => {
+    const handleLogin = async (e) => {
+        e.preventDefault()
         try {
             const res = await axios.post(`${baseURL}/api/login`, {
                 email, password
             });
             localStorage.setItem("token", res.data.token);
-            window.location.href = "/dashboard";
+            navigate('/dashboard')
         } catch (err) {
             alert("Login failed");
         }
@@ -37,6 +40,7 @@ export default function Login() {
                     placeholder="Password"
                 />
                 <button
+                    type="button"
                     onClick={handleLogin}
                     className="w-full bg-blue-600 text-white py-2 rounded"
                 >
